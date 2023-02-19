b675fbdf25a9ae6af50ff0c138e784c701f7a787
Khalil Ahmed <klilax@hotmail.co.uk>
2023-02-19 15:33:45 +0300

    user auth

--- src/fileUpload.js
+++ src/fileUpload.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FormExample() {
     const [formData, setFormData] = React.useState(null);
-    const link = 'http://127.0.0.1:8000/media/upload/1'
+    const url = 'http://127.0.0.1:8000/token'
     const handleSubmit = event => {
         event.preventDefault();
 
@@ -11,19 +11,23 @@ function FormExample() {
         setFormData(data);
     };
 
-    React.useEffect(() => {
-        if (formData) {
-            fetch(link, {
+    React.useEffect( () => {
+        async function fetchData() {
+            const token = await fetch(url, {
                 method: 'POST',
                 body: formData
             });
+            return token.json()
+        }
+        if (formData) {
+            console.log(fetchData())
         }
     }, [formData]);
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" name="name" />
-            <input type="file" name="file" />
+            <input type="text" name="username" />
+            <input type="password" name="password" />
             <button type="submit">Submit</button>
         </form>
     );
