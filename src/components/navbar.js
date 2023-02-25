9f4c789e1da6b1a10389fb17da819d84dc56dbb3
Khalil Ahmed <klilax@hotmail.co.uk>
2023-02-25 11:11:10 +0300

    nav var fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,11 +6,11 @@ function Navbar(props) {
 
   return (
     // <nav className={`flex justify-between items-center h-16 bg-${isDark ? 'gray' : 'green'}-500 text-white`}>
-    <nav className="flex justify-between items-center dark:bg-gray-800 500 text-white">
+    <nav className="flex justify-between items-center text-black dark:bg-gray-800 500 dark:text-white">
       <div className="flex items-center">
         <Link to="/" className="mr-4 text-lg font-bold">Spark</Link>
-        <Link to="/about" className="mr-4v">About</Link>
-        <Link to="/login">Login</Link>
+        <Link to="/about" className="mr-4">About</Link>
+        <Link to="/login" className="mr-4">Login</Link>
       </div>
       <div className="flex items-center">
         {/* eslint-disable-next-line react/prop-types */}
